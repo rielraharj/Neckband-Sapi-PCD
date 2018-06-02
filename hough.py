4a2a231119b32b1a2854ea6e81ca1d65a63ffff3
rielraharj <rielraharj@gmail.com>
2018-06-02 16:52:54 +0700

    add comment and lvq

--- hough.py
+++ hough.py
@@ -6,6 +6,8 @@ import os
 
 sumclass=[0,0,0,0,0,0,0,0,0]
 save_path_pola = "pola kalung"
+
+#Mengambil gambar tiap folder kelas
 for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
     print(class_image_path)
     if (class_image_path.split("\\")[-1] == 'Kelas 1'): neck_class = 1
@@ -24,28 +26,18 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
         print(image_path)
         # if(neck_class!=7):
         #     break
-# for i in range(1,10):
-#         print (i)
-#         x=str(i)
-#         print(x)
-#         print(type(x))
         x=str(neck_class)+"-class-"+str(sumclass[neck_class])
         name = x + "-test.bmp"
         print(name)
         print(type(name))
         im_gray = cv2.imread(image_path,0)
-        #i += 1
         thresh = 127
         im_binerr = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-        # cv2.imshow("img binerr",im_binerr)
         im_gray = cv2.medianBlur(im_gray,5)
-        # cv2.imshow("img medianblur",im_gray)
-
         im_biner = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2BGR)
 
         arr = []
         v = []
-
         if(neck_class==7):
             houghparam=35
         else:
@@ -53,10 +45,8 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
 
         try:
 
-            circles = cv2.HoughCircles(im_gray, cv2.HOUGH_GRADIENT, 1, 100, param1=290, param2=houghparam,
-                                       minRadius=0, maxRadius=0)
+            circles = cv2.HoughCircles(im_gray, cv2.HOUGH_GRADIENT, 1, 100, param1=290, param2=houghparam, minRadius=0, maxRadius=0)
             circles = np.uint16(np.around(circles))
-            # cv2.imshow("hasil", circles)
             for i in circles[0, :]:
                 cv2.circle(im_biner, (i[0], i[1]), i[2], (0, 255, 255), 2)
                 cv2.circle(im_biner, (i[0], i[1]), 2, (0, 0, 255), 112)
@@ -79,22 +69,18 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
             im_hasil = cv2.subtract(graykanvas, im_gray)
 
             hasil_crop = im_hasil[x:x + 112, y - 56:y + 56]  # im awe [y,x]
-
             cv2.imshow("hasil crop", hasil_crop)
             thresh = 130
 
             kernel = np.ones((5, 5), np.uint8)
 
             crop_biner = cv2.threshold(hasil_crop, thresh, 255, cv2.THRESH_BINARY)[1]
-            #cv2.imshow("hasil circle crop biner", crop_biner)
 
 
 
             cv2.imwrite(os.path.join(new_save_path,name),crop_biner)
-            # print("aaa")
-            row, col= crop_biner.shape
-            print(row,col)
 
+            row, col= crop_biner.shape
             for r in range(0,row):
                 a = 0
                 for c in range(0,col):
@@ -107,7 +93,6 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
             print(len(v))
             print("tipe",type(v))
             print(v)
-            # max=max(v)
             v=v/max(v)
             v=[int(round(l)) for l in v]
 
@@ -116,16 +101,8 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
                 arr.append(d)
             arr.append(neck_class)
             print(arr)
-            # wibu.append(arr[0])
-            # for i in arr[1]:
-            #     wibu.append(i)
-            # wibu.append(arr[2])
-            #
-            # print(wibu)
 
 
-
-            # arr_np = np.array(arr)
             csvfile = "datavector.csv"
 
             with open(csvfile, 'a+',newline='') as output:
@@ -136,14 +113,6 @@ for class_image_path in glob.glob("D:\PycharmProjects\PCDSAPI\kalung sapi\*"):
                 writer.writerow([arr[-1]])
 
             sumclass[neck_class]=sumclass[neck_class]+1
-            # csv.writer("vectorkalung")
-            #
-            # np.savetxt("datavectorkalung.csv",arr,delimiter=",")
-
-            # img_dilation = cv2.dilate(crop_biner, kernel, iterations=1)
-            # cv2.imshow("dilasi", img_dilation)
-            # img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
-            # cv2.imshow("akhir", img_erosion)
 
         except Exception:
             pass
